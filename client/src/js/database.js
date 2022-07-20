27adfad13b7237f60e661875969024087e4aacff
Nate Traugh <n.traugh@yahoo.com>
2022-07-20 14:08:03 -0400

    staged for heroku

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,14 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (id, content) => console.error('putDb not implemented');
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async (content) => {
+  console.log("Grabbing data from JATEDB")
+  const jateDb = await openDB("jate", 1)
+  const transaction = jateDb.transaction("jate", "readwrite")
+  const obj = tx.object
+};
 
 initdb();
