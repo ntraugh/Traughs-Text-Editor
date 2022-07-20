5cef580678600debbfc1df0e57bbafcae409320e
Nate Traugh <n.traugh@yahoo.com>
2022-07-20 12:42:59 -0400

    added StaleWhileRevalidate to our registerRoute strategy

--- client/src-sw.js
+++ client/src-sw.js
@@ -4,7 +4,7 @@ const { registerRoute } = require('workbox-routing');
 const { CacheableResponsePlugin } = require('workbox-cacheable-response');
 const { ExpirationPlugin } = require('workbox-expiration');
 const { precacheAndRoute } = require('workbox-precaching/precacheAndRoute');
-
+const { StaleWhileRevalidate } = require('workbox-strategies')
 precacheAndRoute(self.__WB_MANIFEST);
 
 const pageCache = new CacheFirst({
@@ -28,6 +28,11 @@ registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
 // TODO: Implement asset caching
 registerRoute(
-  ({ request }) => ["style", "script", "worker"].includes(request.destination)
-  
+  ({ request }) => ["style", "script", "worker"].includes(request.destination),
+  new StaleWhileRevalidate ({ cacheName: "asset-cache",
+  plugins: [
+    new CacheableResponsePlugin({
+      statuses: [0, 200]
+    })
+  ]})
 );
